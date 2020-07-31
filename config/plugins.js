ee91762df00c77885618c49dd8c6ccca4c749cff
Ali Klein <alexandraeklein@yahoo.com>
2020-07-31 13:08:23 -0700

    files

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: "dyr2npas7",
+      api_key: "322828629415446",
+      api_secret: "vsZz8L4wmS3gM8JrPsrI8pPGl5w",
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5a241dcc-0262-4dc0-b965-694639d2d6be'
+};
