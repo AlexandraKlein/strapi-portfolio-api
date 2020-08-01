a4a8cf9ed0cbeb25e27c5465d6e607ca9d14ce05
Ali Klein <alexandraeklein@yahoo.com>
2020-08-01 11:41:04 -0700

    database js revert

--- config/database.js
+++ config/database.js
@@ -4,21 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: env(
-          "DATABASE_HOST",
-          "ec2-54-197-254-117.compute-1.amazonaws.com"
-        ),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "d70m5ju8r96jh0"),
-        username: env("DATABASE_USERNAME", "ptalmtcptcrkzk"),
-        password: env(
-          "DATABASE_PASSWORD",
-          "d475909d992294ed49f27701ed90b7f8216a3760208b47abd5d2e313d0f2d18f"
-        ),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
-        ssl: false,
+        useNullAsDefault: true,
       },
     },
   },
